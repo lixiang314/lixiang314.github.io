6608540b969eaa565cfcc2af5a2f3687dfb3ac0d
条件状语从句 <lixzap@gmail.com>
2016-04-07 17:03:15 +0800

    gengxin

--- tinyweather/js/index.js
+++ tinyweather/js/index.js
@@ -151,19 +151,19 @@ function getInfo() {
 	});
 }
 
-// 获得背景图片，使用知乎日报api
-$.ajax({
-  url:'http://news-at.zhihu.com/api/4/start-image/1080*1776',
-  dataType : 'json',
-  type : "get",
-  async : false,
-  dataType:"json",
+// // 获得背景图片，使用知乎日报api
+// $.ajax({
+//   url:'http://news-at.zhihu.com/api/4/start-image/1080*1776',
+//   dataType : 'json',
+//   type : "get",
+//   async : false,
+//   dataType:"json",
 
-  success:function (data) {
-    $('.backimg').css('background','url('+data.img+') center / contain no-repeat')
+//   success:function (data) {
+//     $('.backimg').css('background','url('+data.img+') center / contain no-repeat')
 
-  }
-})
+//   }
+// })
 
 
 // 将ASCII编码转化为汉字
